68a89f4f7830cd2ad0dec9fbf4aaadb5d5163a53
John Krone <john.krone@kellogg.northwestern.edu>
2023-04-19 20:17:17 +0000

    updated structure of code to fit assignment

--- kmdb.sql
+++ kmdb.sql
@@ -105,37 +105,12 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
-
--- Create new tables, according to your domain model
--- TODO!
-
--- Insert data into your database that reflects the sample data shown above
--- Use hard-coded foreign key IDs when necessary
--- TODO!
-
--- Prints a header for the movies output
-.print "Movies"
-.print "======"
-.print ""
-
--- The SQL statement for the movies output
--- TODO!
-
--- Prints a header for the cast output
-.print ""
-.print "Top Cast"
-.print "========"
-.print ""
-
-
--- The SQL statement for the cast output
--- TODO!
-
 DROP TABLE movies IF EXISTS;
 DROP TABLE actors IF EXISTS;
 DROP TABLE studios IF EXISTS;
 DROP TABLE characters IF EXISTS;
-
+-- Create new tables, according to your domain model
+-- TODO!
 CREATE TABLE (movie
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 title TEXT,
@@ -160,4 +135,27 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 movie_id INTEGER,
 actor_id INTEGER
 );
+
+-- Insert data into your database that reflects the sample data shown above
+-- Use hard-coded foreign key IDs when necessary
+-- TODO!
+
+-- Prints a header for the movies output
+.print "Movies"
+.print "======"
+.print ""
+
+-- The SQL statement for the movies output
+-- TODO!
+
+-- Prints a header for the cast output
+.print ""
+.print "Top Cast"
+.print "========"
+.print ""
+
+
+-- The SQL statement for the cast output
+-- TODO!
+
 
